b7b9fecdd3f9d45f9c7b403623d5bb4b35f37b35
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2024-01-18 00:00:06 -0300

    c1/ stepp105 / 32% - 60%

--- script.js
+++ script.js
@@ -105,7 +105,13 @@ function buyWeapon() {
   }
 }
 
-
+function sellWeapon() {
+  if (inventory.length > 1) {
+    gold += 15;
+    goldText.innerText = gold;
+    let currentWeapon
+  }
+}
 
 function fightSlime() {
 
