98e80ea22cb108a3717620e487ef52fc85ed7302
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2024-01-24 21:18:43 -0300

    c1-3/ step 74 - 81 / 87% - 95%

--- script.js
+++ script.js
@@ -158,7 +158,22 @@ const deleteSong = (id) => {
   setPlayButtonAccessibleText(); 
 
   if (userData.songs.length === 0) {
-  const resetButton = document.createElement('button')
+    const resetButton = document.createElement("button");
+    const resetText = document.createTextNode("Reset Playlist");
+
+    resetButton.id = "reset";
+    resetButton.ariaLabel = "Reset playlist";
+    resetButton.appendChild(resetText);
+    playlistSongs.appendChild(resetButton);
+
+    resetButton.addEventListener("click", () => {
+      userData.songs = [...allSongs];
+
+      renderSongs(userData?.songs); 
+      setPlayButtonAccessibleText();
+      resetButton.remove();
+    });
+
   }
 
 };
@@ -235,4 +250,9 @@ previousButton.addEventListener("click", playPreviousSong);
 
 shuffleButton.addEventListener("click", shuffle);
 
+audio.addEventListener("ended", () => {
+});
+
+
 renderSongs(userData?.songs);
+setPlayButtonAccessibleText();
